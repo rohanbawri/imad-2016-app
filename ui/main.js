3abd62d8e510c380b6ec2c65af7cfa1d72c8596c
rohanbawri <rohan.bawri@gmail.com>
2016-10-06 02:24:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = "New value";
+
+var img = document.getElementById("madi");
+
+img.onclick = function(){
+    img.style.marginleft = 100 px;
+}
