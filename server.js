d5fdc9a31cb473cf95ad84f57cb2d19064ca18a0
rohanbawri <rohan.bawri@gmail.com>
2016-10-06 02:04:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/:articleName',function (req,res) {
  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/style.caa', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
