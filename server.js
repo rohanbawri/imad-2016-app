3b988cc56d5464838436b712f8783f92253812a5
rohanbawri <rohan.bawri@gmail.com>
2016-10-06 01:22:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,11 +2,55 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articleOne = {
+    title: 'Article one | rohan bawri',
+    heading: "Article one",
+    date: 'Sep 5, 2016',
+    content: 
+         `<p>
+            This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. This is content of article one. 
+          </p> `
+};
+function createTemplate(data){
+    
+            var title = data.title;
+            var heading = data.heading;
+            var date = data.date;
+            var content = data.content;
+            var htmlTemplate = `
+                      <html>
+                <head>
+                    <title>${title}</title>
+                    <link href="/ui/style.css" rel="stylesheet" />
+                <body>
+                    <div class= "container">
+                    <div>
+                        <a href='/'>Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                       ${date}
+                    </div>
+                    <p>
+                        ${content}
+                    </p>
+                    </div>
+                </body>
+            </html>
+            
+            `;
+            return htmlTemplate;
+}
+
+
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-one',function (req,res) {
