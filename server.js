868b55bc13f3a2bc245d84575bc0d6d65fcbb71b
rohanbawri <rohan.bawri@gmail.com>
2016-10-07 17:25:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter =0;
 app.get('/counter',function(req,res){
    counter = counter +1;
    res.send(counter.toString());
