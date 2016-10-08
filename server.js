927e2d6868a56b3a063cb14604ebf23a09910622
rohanbawri <rohan.bawri@gmail.com>
2016-10-08 21:54:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+
 var app = express();
 app.use(morgan('combined'));
+
 
 var articles = {
       'article-one' : {
