ac3a4e64c85473d9c980cb4db265252747fee689
rohanbawri <rohan.bawri@gmail.com>
2016-10-08 20:38:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,17 @@ button.onclick = function(){
   
     
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick= function(){
+    
+    
+    var names= ['Name1','Name2','Name3','Name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
