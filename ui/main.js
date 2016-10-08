ccceb1c6bc22534ed04ba36d2ea1d77ca1be0b10
rohanbawri <rohan.bawri@gmail.com>
2016-10-08 22:06:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,12 +27,28 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick= function(){
     
+    var request = new XMLHttpRequest();
     
-    var names= ['Name1','Name2','Name3','Name4'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status ===200){
+                    var names= request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                    for(var i=0;i<names.length;i++){
+                        list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                                
+            }
+            
+        }
+        
+    };
+     
+    request.open('GET','http://rohanbawri.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
+  
+   
 };
